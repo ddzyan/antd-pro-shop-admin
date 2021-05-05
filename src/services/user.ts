65c5d7119b876321a3f5326eeb883003bca0c43e
dingdongzhao <dingdongzhao@didiglobal.com>
2021-05-05 14:45:31 +0800

    fist commit

---
+++ src/services/user.ts
@@ -0,0 +1,14 @@
+import request from '@/utils/request';
+
+export async function query(): Promise<any> {
+  return request('/api/users');
+}
+
+export async function queryCurrent(): Promise<any> {
+  return request('/api/currentUser');
+  // return request.post('/auth/login');
+}
+
+export async function queryNotices(): Promise<any> {
+  return request('/api/notices');
+}
